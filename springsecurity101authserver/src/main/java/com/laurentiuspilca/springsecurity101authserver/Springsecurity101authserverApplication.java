011d45ef94ff4a96abf2d4ffcd6c1964d6b622f9
Laurentiu Spilca <laurentiu.spilca@endava.com>
2019-06-02 13:40:28 +0300

    Spring security 101 auth server

---
+++ springsecurity101authserver/src/main/java/com/laurentiuspilca/springsecurity101authserver/Springsecurity101authserverApplication.java
@@ -0,0 +1,13 @@
+package com.laurentiuspilca.springsecurity101authserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springsecurity101authserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springsecurity101authserverApplication.class, args);
+    }
+
+}
---
+++ springsecurity101authserver/src/main/java/com/laurentiuspilca/springsecurity101authserver/config/AuthorizationServerConfig.java
@@ -0,0 +1,33 @@
+package com.laurentiuspilca.springsecurity101authserver.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+
+@Configuration
+@EnableAuthorizationServer
+@EnableResourceServer
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                .withClient("clientId")
+                .secret("abcde")
+                .authorizedGrantTypes("password", "refresh_token", "authorization_token")
+                .scopes("write");
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.authenticationManager(authenticationManager);
+    }
+}

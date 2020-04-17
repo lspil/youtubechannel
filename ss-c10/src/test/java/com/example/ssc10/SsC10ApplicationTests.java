281d5e135e98244617f535d510cc49e222ff2ae5
Laurentiu Spilca <laurentiu.spilca@endava.com>
2020-04-17 20:25:08 +0300

    SS 10

---
+++ ss-c10/src/test/java/com/example/ssc10/SsC10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ssc10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsC10ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

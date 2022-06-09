58d6ecf8e49e9cd3abe23a6c91ed8f28e1cda854
mkwiecin <mkwiecin@poczta.onet.pl>
2022-06-09 13:52:46 +0200

    after test

--- ss-c5/src/main/java/com/laurentiuspilca/ssc5/security/authentication/CustomAuthentication.java
+++ ss-c5/src/main/java/com/laurentiuspilca/ssc5/security/authentication/CustomAuthentication.java
@@ -5,8 +5,7 @@ import org.springframework.security.core.GrantedAuthority;
 
 import java.util.Collection;
 
-public class CustomAuthentication
-        extends UsernamePasswordAuthenticationToken {
+public class CustomAuthentication extends UsernamePasswordAuthenticationToken {
 
     public CustomAuthentication(Object principal, Object credentials) {
         super(principal, credentials);
